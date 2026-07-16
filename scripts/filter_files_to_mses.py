513c5cb63f08a888d81742ad1979494df6db7446
Frits Sweijen <tikk3r@users.noreply.github.com>
2026-07-16 12:37:45 +0100

    Trim model images (#142)
    Fixes #138 
    
    A selection of delay calibrator candidates can be made based on the
    phasediff score, but the corresponding array of starting model images is
    not yet trimmed to this same amount. This causes a crash in case the
    number of candidates to select doesn't match the number of input model
    images.
    
    This PR adds an extra step before in the self calibration to ensure
    there is an equal number of model images.
    
    ---------
    
    Co-authored-by: Matthijs van der Wild <lonbar@users.noreply.github.com>

---
+++ scripts/filter_files_to_mses.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+from argparse import ArgumentParser
+
+from submods.source_selection.selfcal_selection import parse_source_from_h5
+
+
+def main():
+    parser = ArgumentParser(
+        description=(
+            "Filters a given set of files into one that matches the set of MSes."
+            " This is done by extracting the source name, so both MSes and files"
+            " are expected to follow the ILTJ... naming convention."
+        )
+    )
+    parser.add_argument(
+        "--ms",
+        type=str,
+        nargs="+",
+        help="MSes to use as reference for filtering the files.",
+    )
+    parser.add_argument(
+        "--files",
+        type=str,
+        nargs="*",
+        help="Files to filter a set matching the MSes from.",
+    )
+    args = parser.parse_args()
+
+    if not args.files:
+        with open("cwl.output.json", "w") as f:
+            json.dump({"filtered_files": None}, f)
+        return
+
+    source_names_ms = {parse_source_from_h5(ms) for ms in args.ms}
+
+    keep_files = [f for f in args.files if parse_source_from_h5(f) in source_names_ms]
+
+    if not keep_files:
+        raise RuntimeError("No files in common with the MSes. Please check inputs.")
+    elif len(keep_files) != len(source_names_ms):
+        raise NotImplementedError(
+            "Number of files does not match number of MSes. This is not supported."
+        )
+
+    cwl_files = [{"class": "File", "path": f} for f in keep_files]
+
+    with open("cwl.output.json", "w") as f:
+        json.dump({"filtered_files": cwl_files}, f)
+
+
+if __name__ == "__main__":
+    main()
