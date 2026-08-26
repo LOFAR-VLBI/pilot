7681e8b64bfce3101fd4096f9347b659a54e271f
jurjen93 <jurjendejong93@gmail.com>
2026-08-26 14:12:23 +0200

    refactor

--- scripts/make_config_dical.py
+++ scripts/make_config_dical.py
@@ -97,7 +97,7 @@ def make_config(best_solint: float, smoothness: float, imagecat: str, inputmodel
 
     # Add Leakage calibration if requested
     if leakagecal:
-        configdict['makeimage_fullpol'] = True
+        configdict['makeimage_fullpol'] = 'True'
         if peak_flux is None or peak_flux <= 1:
             configdict['soltypecycles_list'].extend([soltypecycle_fulljones, soltypecycle_fulljones])
             configdict['solint_list'].extend([amplitude_solint, amplitude_solint])
