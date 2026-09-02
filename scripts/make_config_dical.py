62c4d13dd1cac7122c42ef5060b89cde09c33f6e
jurjen93 <jurjendejong93@gmail.com>
2026-09-02 11:57:21 +0200

    return fix

--- scripts/make_config_dical.py
+++ scripts/make_config_dical.py
@@ -202,14 +202,15 @@ def process_catalog(imagecat: str, ms: str) -> tuple[bool, bool, float]:
         bandpass: bool
         phaseup: bool
     """
-    im_t = Table.read(imagecat)
 
     bandpass = False # Default option
     phaseup = True # Default option
 
     if not im_t:
-        return bandpass, phaseup
-    
+        return bandpass, phaseup, 0
+    else:
+        im_t = Table.read(imagecat)
+
     with ct.table(f"{ms}/FIELD", readonly=True, ack=False) as field_table:
         phase_dir = field_table.getcol('PHASE_DIR')[0, 0]  # shape: (n_fields, 1, 2)
         ra_rad, dec_rad = phase_dir
