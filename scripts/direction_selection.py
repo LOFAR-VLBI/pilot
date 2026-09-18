96967afdab3d8355de3293af742be00ea6ef0fa4
Frits Sweijen <tikk3r@users.noreply.github.com>
2026-09-18 14:10:45 +0100

    Allow close-by sources to be kept

--- scripts/direction_selection.py
+++ scripts/direction_selection.py
@@ -14,7 +14,7 @@ from astropy import units as u
 from make_config_international import parse_source_id
 
 
-def filter_too_nearest_neighbours(csv: str, sep: float = 0.06) -> pd.DataFrame:
+def filter_too_nearest_neighbours(csv: str, sep: float = 0.06, keep_close_sources: bool = False) -> pd.DataFrame:
     """
     Identify sources that have a nearest neighbour within 0.1 degrees distance.
     Keep the source with the highest spd_score.
@@ -22,6 +22,8 @@ def filter_too_nearest_neighbours(csv: str, sep: float = 0.06) -> pd.DataFrame:
     Args:
         csv: CSV file with RA/DEC and spd_score
         sep: separation threshold in degrees
+        keep_close_sources: do not remove sources too close to a calibrator,
+            but give them a poor score such that they can be imaged with nearest solutions.
 
     Returns: DataFrame filtered on spd_score and seperation threshold
     """
@@ -52,11 +54,15 @@ def filter_too_nearest_neighbours(csv: str, sep: float = 0.06) -> pd.DataFrame:
                 to_remove.add(i)
 
     # Remove rows marked for removal
-    print('Removing:')
-    print(df.iloc[list(to_remove)])
-    filtered_df = df.drop(index=to_remove).reset_index(drop=True)
+    if not keep_close_sources:
+        print('Removing:')
+        print(df.iloc[list(to_remove)])
+        filtered_df = df.drop(index=to_remove).reset_index(drop=True)
+        return filtered_df
+    else:
+        df.loc[list(to_remove), 'spd_score'] = 10.0
+        return df
 
-    return filtered_df
 
 
 def match_source_id(mslist: list, source_id: str) -> str:
@@ -120,6 +126,7 @@ def parse_args():
     parser.add_argument('--suffix', help='Suffix to add to MSes after phasediff selection. In case of reclassification this is the new suffix.', default='_best')
     parser.add_argument('--reclassify_from', help='Suffix that will be rename to that given by --suffix.', default='')
     parser.add_argument('--min-separation', help='Minimum allowed separation in degrees between sources. The best scoring source is kept.', default=0.06)
+    parser.add_argument('--keep-close-sources', help='Keeps sources that are too close to a calibrator such that they can still be imaged with the nearest solutions.', default='')
     return parser.parse_args()
 
 
@@ -138,7 +145,7 @@ def main():
         if args.select_best_N > 0:
             df = df.head(args.select_best_N)
         else:
-            df = filter_too_nearest_neighbours(args.csv, sep=args.min_separation)
+            df = filter_too_nearest_neighbours(args.csv, sep=args.min_separation, keep_close_sources=args.keep_close_sources)
             df = df.sort_values("spd_score", ascending=True)
             if len(df) < args.select_best_N:
                 print(f"Warning: {args.select_best_N} sources requested, but only {len(df)} sources present.")
