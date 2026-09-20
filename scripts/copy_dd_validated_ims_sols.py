708a2281a50807289502a447fb934c5dafaa311f
Frits Sweijen <tikk3r@users.noreply.github.com>
2026-09-20 15:00:53 +0100

    Do not crash if allowed rejected fraction is 1

--- scripts/copy_dd_validated_ims_sols.py
+++ scripts/copy_dd_validated_ims_sols.py
@@ -83,7 +83,8 @@ def main():
     # Cases where images are accepted but bad solutions. This may indicate unstable calibration for good high S/N calibrators
     print(f"Warning: Following directions are rejected and should be inspected: \n{'\n'.join(list(sources_with_bad_solutions.source_id))}")
 
-    if len(validation_csv) == 0:
+    if (len(validation_csv) == 0) and (args.max_rejected_fraction < 1):
+
         raise ValueError("No directions left whose images passed validation.")
     elif len(sources_with_bad_solutions)/len(validation_csv)>args.max_rejected_fraction:
         exit(f"ERROR: {int(len(sources_with_bad_solutions)/len(validation_csv)*100)}% directions are rejected, which exceeds the "
